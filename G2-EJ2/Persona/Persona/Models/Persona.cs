7784fd582494a1de704e69ab8fdfc80f8be02293
chris <chrisdesanfrancisco@gmail.com>
2026-07-09 16:07:06 -0600

    Add G2-EJ2 completed

---
+++ G2-EJ2/Persona/Persona/Models/Persona.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Persona.Models
+{
+    public class Persona : IValidatableObject
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime FechaDeNacimiento { get; set; }
+        public string Direccion { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (FechaDeNacimiento > DateTime.Today.AddYears(-18))
+            {
+                yield return new ValidationResult("Age must be above 18.", new[] { nameof(FechaDeNacimiento) });
+            }
+        }
+    }
+}
