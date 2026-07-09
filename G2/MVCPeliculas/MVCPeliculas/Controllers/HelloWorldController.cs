598dc51687f506cb7c2f73afdeb34c7291909847
chris <chrisdesanfrancisco@gmail.com>
2026-07-09 15:51:21 -0600

    Add completed 'guía 2'

---
+++ G2/MVCPeliculas/MVCPeliculas/Controllers/HelloWorldController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MVCPeliculas.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Welcome(string nombre, string apellido, int numVeces = 1)
+        {
+            ViewData["nombre"] = nombre;
+            ViewData["numVeces"] = numVeces;
+            ViewData["apellido"] = apellido;
+            return View();
+        }
+
+
+        public string Greeting(string nombre, int id = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hola {nombre}, ID: {id}");
+        }
+    }
+}
---
+++ G2/MVCPeliculas/MVCPeliculas/Data/PeliculasDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCPeliculas.Models;
+
+namespace MVCPeliculas.Data
+{
+    public class PeliculasDbContext: DbContext
+    {
+
+        public PeliculasDbContext(DbContextOptions options): base(options) { }
+
+        public DbSet<Pelicula> Peliculas { get; set; }
+    }
+}
